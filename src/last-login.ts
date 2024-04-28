802424c6ffe095798cb85a88b0644dbc2841d82d
ryooo <ryooo.321@gmail.com>
2024-04-28 14:16:37 +0900

    feat: use artifact

---
+++ src/last-login.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs'
+
+import { teamAccessLogs } from './slack'
+
+/**
+ * { slackUserName: lastLoginDate }
+ */
+export type LastLogin = { [key: string]: number }
+
+export const getArtifactLastLogin = (): LastLogin => {
+  try {
+    return JSON.parse(fs.readFileSync('last-login.json', 'utf-8'))
+  } catch (e) {
+    console.error(e)
+  }
+  return {}
+}
+
+export const getAndUpdateLastLogin = async (
+  slackUserToken: string
+): Promise<LastLogin> => {
+  const lastLogin = getArtifactLastLogin()
+
+  const teamAccessLogsResponse = await teamAccessLogs(slackUserToken)
+  if (!teamAccessLogsResponse.ok) {
+    console.error(`teamAccessLogs has error. ${teamAccessLogsResponse.error}`)
+  } else {
+    for (const login of teamAccessLogsResponse.logins ?? []) {
+      if (login.username && login.date_last) {
+        lastLogin[login.username] = Math.max(
+          lastLogin[login.username] ?? 0,
+          login.date_last
+        )
+      }
+    }
+  }
+
+  fs.writeFileSync('last-login.json', JSON.stringify(lastLogin))
+  return lastLogin
+}
